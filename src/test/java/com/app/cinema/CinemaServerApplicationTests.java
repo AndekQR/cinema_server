ce7f9b55d33d7c7dae8bc1e654e0d71407956b80
Daniel <daniellegaiec20@gmail.com>
2020-10-08 17:23:27 +0200

    first commit

---
+++ src/test/java/com/app/cinema/CinemaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
